e58d41f50fcdfdc38ffcc7d6df7e752842d0b38c
Do Tran Thai An <justapie06@gmail.com>
2025-12-15 12:09:45 +0700

    feat: add config and jwt_utils

---
+++ src/config.rs
@@ -0,0 +1,27 @@
+use std::{env, sync::OnceLock};
+
+pub struct Config {
+    pub listen_address: String,
+    pub jwks_url: String,
+}
+
+impl Config {
+    pub fn initialize() -> Self {
+        Self {
+            listen_address: env::var("LISTEN_ADDRESS")
+                .unwrap_or(String::from(DEFAULT_LISTEN_ADDRESS)),
+            jwks_url: env::var("JWKS_URL").expect("Missing JWKS_URL environment variable"),
+        }
+    }
+}
+
+static DEFAULT_LISTEN_ADDRESS: &str = "0.0.0.0:3000";
+static INSTANCE: OnceLock<Config> = OnceLock::new();
+
+pub fn get_cell() -> &'static OnceLock<Config> {
+    &INSTANCE
+}
+
+pub fn get() -> &'static Config {
+    &INSTANCE.get().expect("config should be initialized")
+}
---
+++ src/jwt_utils.rs
@@ -0,0 +1,60 @@
+use std::{error::Error, sync::OnceLock};
+
+use jsonwebtoken::Validation;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Claims {
+    sub: String,
+}
+
+pub struct JwtUtils {
+    jwks_client: jwks::Jwks,
+    validation: Validation,
+}
+
+impl JwtUtils {
+    pub async fn initialize(jwks_url: String) -> Self {
+        Self {
+            jwks_client: jwks::Jwks::from_jwks_url(jwks_url)
+                .await
+                .expect("Invalid JWKS_URL"),
+            validation: Validation::default(),
+        }
+    }
+
+    pub fn verify(&self, jwt: &str) -> Result<String, Box<dyn Error>> {
+        let header = match jsonwebtoken::decode_header(jwt) {
+            Ok(v) => v,
+            Err(e) => return Err(e.into()),
+        };
+
+        let kid = match header.kid {
+            Some(v) => v,
+            None => return Err("".into()),
+        };
+
+        let jwk = match self.jwks_client.keys.get(&kid) {
+            Some(v) => v,
+            None => return Err("".into()),
+        };
+
+        let decode = match jsonwebtoken::decode::<Claims>(&jwt, &jwk.decoding_key, &self.validation)
+        {
+            Ok(v) => v,
+            Err(e) => return Err(e.into()),
+        };
+
+        Ok(decode.claims.sub)
+    }
+}
+
+static INSTANCE: OnceLock<JwtUtils> = OnceLock::new();
+
+pub fn get_cell() -> &'static OnceLock<JwtUtils> {
+    &INSTANCE
+}
+
+pub fn get() -> &'static JwtUtils {
+    &INSTANCE.get().expect("jwt_utils should be initialized")
+}
