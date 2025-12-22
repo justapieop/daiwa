b7cb3b873c0ca31e90e782b56f8560b18ee1536b
Do Tran Thai An <justapie06@gmail.com>
2025-12-22 13:13:19 +0700

    chore: make create room event

--- src/events.rs
+++ src/events.rs
@@ -4,7 +4,7 @@ use std::{
     result,
 };
 
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use socketioxide::{
     extract::{Data, Extension, SocketRef, State},
     socket::DisconnectReason,
@@ -19,10 +19,10 @@ pub struct UserData {
     user_id: String,
 }
 
-#[derive(Debug, Validate, Deserialize)]
+#[derive(Debug, Validate, Clone, Copy, Serialize, Deserialize)]
 struct RoomCreateRequest {
     #[validate(range(min = 3, max = 50))]
-    player_number: u8,
+    pub player_number: u8,
 }
 
 #[derive(Debug)]
@@ -34,11 +34,7 @@ impl Display for AuthError {
 }
 impl Error for AuthError {}
 
-pub async fn on_connect(
-    socket: SocketRef,
-    State(state): State<AppState>,
-    Extension(ext): Extension<UserData>,
-) {
+pub async fn on_connect(socket: SocketRef, Extension(ext): Extension<UserData>) {
     info!("Connection from user {}", ext.user_id);
 
     socket.on_disconnect(
@@ -50,7 +46,13 @@ pub async fn on_connect(
     socket.on("room_create_request", on_client_room_create_request);
 }
 
-async fn on_client_room_create_request(socket: SocketRef, Data(data): Data<RoomCreateRequest>) {
+async fn on_client_room_create_request(
+    socket: SocketRef,
+    Data(data): Data<RoomCreateRequest>,
+    State(state): State<AppState>,
+    Extension(ext): Extension<UserData>,
+) {
+    info!("Room create request from user {}", ext.user_id);
     match data.validate() {
         Ok(_) => {}
         Err(e) => {
@@ -58,6 +60,35 @@ async fn on_client_room_create_request(socket: SocketRef, Data(data): Data<RoomC
             return;
         }
     }
+
+    if !socket.rooms().is_empty() {
+        info!("User {} already in a room", ext.user_id);
+        socket
+            .broadcast()
+            .emit("already_in_room", "You are already in a room")
+            .await
+            .unwrap_or_default();
+        return;
+    }
+
+    let id: u128 = state.snowflake.lock().await.next_id().await;
+
+    socket.join([id.to_string()]);
+
+    let room = state
+        .session_manager
+        .lock()
+        .await
+        .create_room(id, data.player_number);
+
+    let res = serde_json::to_string(&room).unwrap_or_default();
+
+    info!(
+        "Room {} created for user {}. Automatically joining...",
+        id, ext.user_id
+    );
+
+    socket.emit("room_created", &res).unwrap_or_default();
 }
 
 pub async fn verify_auth_header(
