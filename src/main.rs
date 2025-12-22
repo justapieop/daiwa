34ae81b2b3dc5e377039b0252d7091571e113672
Do Tran Thai An <justapie06@gmail.com>
2025-12-22 13:13:31 +0700

    chore: add more log message

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod config;
 mod events;
 mod jwt_utils;
 mod logging;
+mod session;
 mod snowflake;
 
 use std::sync::Arc;
@@ -15,12 +16,14 @@ use tokio::sync::{Mutex, OnceCell};
 use tower_http::{compression::CompressionLayer, cors::CorsLayer, trace::TraceLayer};
 use tracing::info;
 
+use crate::session::SessionManager;
 use crate::{jwt_utils::JwtUtils, snowflake::Snowflake};
 
 #[derive(Clone)]
 struct AppState {
     snowflake: Arc<Mutex<Snowflake>>,
     jwt_utils: Arc<JwtUtils>,
+    session_manager: Arc<Mutex<SessionManager>>,
 }
 
 static CONFIG: OnceCell<config::Config> = OnceCell::const_new();
@@ -32,7 +35,9 @@ pub fn get_config() -> &'static config::Config {
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenvy::dotenv().unwrap_or_default();
-    CONFIG.set(config::Config::new()).unwrap();
+    CONFIG
+        .set(config::Config::new())
+        .expect("config should be set");
     let config = get_config();
     logging::setup(
         &std::env::var("LOG_LEVEL").unwrap_or(String::from("info")),
@@ -46,7 +51,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let state = AppState {
         snowflake: Arc::new(Mutex::const_new(Snowflake::new())),
         jwt_utils: Arc::new(JwtUtils::new(config.jwks_url.clone()).await),
+        session_manager: Arc::new(Mutex::const_new(SessionManager::new())),
     };
+
+    info!("JWK verified successfully. Setting up Socket.IO layer");
 
     let (layer, io) = SocketIoBuilder::new()
         .with_state(state.clone())
@@ -54,13 +62,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     io.ns("/", events::on_connect.with(events::verify_auth_header));
 
+    info!("Initializing axum server");
+
     let app: Router<()> = axum::Router::new()
         .layer(layer)
         .layer(TraceLayer::new_for_http())
         .layer(CompressionLayer::new())
         .layer(CorsLayer::new().max_age(Duration::from_secs(604800)));
 
-    info!("Starting WS server");
+    info!("Starting server");
 
     let listener = TcpListener::bind(config.listen_address.clone())
         .await
