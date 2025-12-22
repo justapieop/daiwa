159d9e26be08ae4af49ee15eca3c489702c33235
Do Tran Thai An <justapie06@gmail.com>
2025-12-22 10:05:54 +0700

    refactor: use socketioxide state to handle jwt_utils and snowflake

--- src/events.rs
+++ src/events.rs
@@ -1,5 +1,92 @@
-use std::any;
+use std::{
+    error::Error,
+    fmt::{Display, Formatter, Result},
+    result,
+};
 
-use socketioxide::extract::SocketRef;
+use serde::Deserialize;
+use socketioxide::{
+    extract::{Data, Extension, SocketRef, State},
+    socket::DisconnectReason,
+};
+use tracing::info;
+use validator::Validate;
 
-pub async fn on_connect(socket: SocketRef) {}
+use crate::AppState;
+
+#[derive(Debug, Clone)]
+pub struct UserData {
+    user_id: String,
+}
+
+#[derive(Debug, Validate, Deserialize)]
+struct RoomCreateRequest {
+    #[validate(range(min = 3, max = 50))]
+    player_number: u8,
+}
+
+#[derive(Debug)]
+pub struct AuthError;
+impl Display for AuthError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "AuthError")
+    }
+}
+impl Error for AuthError {}
+
+pub async fn on_connect(
+    socket: SocketRef,
+    State(state): State<AppState>,
+    Extension(ext): Extension<UserData>,
+) {
+    info!("Connection from user {}", ext.user_id);
+
+    socket.on_disconnect(
+        async |_: SocketRef, reason: DisconnectReason, Extension(ext): Extension<UserData>| {
+            info!("User {} disconnected. Reason {}", ext.user_id, reason);
+        },
+    );
+
+    socket.on("room_create_request", on_client_room_create_request);
+}
+
+async fn on_client_room_create_request(socket: SocketRef, Data(data): Data<RoomCreateRequest>) {
+    match data.validate() {
+        Ok(_) => {}
+        Err(e) => {
+            socket.emit("error", &e.clone()).unwrap_or_default();
+            return;
+        }
+    }
+}
+
+pub async fn verify_auth_header(
+    s: SocketRef,
+    State(state): State<AppState>,
+) -> result::Result<(), AuthError> {
+    let headers = &s.req_parts().headers;
+    let auth_header = match headers.get("Authorization") {
+        Some(v) => v.to_str().unwrap_or_default(),
+        None => return Err(AuthError),
+    };
+
+    if !auth_header.starts_with("Bearer ") {
+        return Err(AuthError);
+    }
+
+    let tokens: Vec<&str> = auth_header.split(" ").collect();
+
+    if tokens.len() < 2 {
+        return Err(AuthError);
+    }
+
+    let jwt = tokens[1];
+    let sub = match state.jwt_utils.verify(jwt) {
+        Ok(v) => v,
+        Err(_) => return Err(AuthError),
+    };
+
+    s.extensions.insert(UserData { user_id: sub });
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,28 @@ mod config;
 mod events;
 mod jwt_utils;
 mod logging;
+mod snowflake;
 
-use std::{
-    error::Error,
-    sync::Arc,
-    time::Duration,
-};
+use std::sync::Arc;
+use std::{error::Error, time::Duration};
 
-use axum::{
-    Router,
-    body::Body,
-    http::{HeaderValue, Request, Response},
-};
-use reqwest::StatusCode;
-use socketioxide::SocketIo;
+use axum::Router;
+use socketioxide::SocketIoBuilder;
+use socketioxide::handler::ConnectHandler;
 use tokio::net::TcpListener;
-use tokio::sync::OnceCell;
-use tower_http::{
-    auth::AsyncRequireAuthorizationLayer, compression::CompressionLayer, cors::CorsLayer,
-    trace::TraceLayer,
-};
+use tokio::sync::{Mutex, OnceCell};
+use tower_http::{compression::CompressionLayer, cors::CorsLayer, trace::TraceLayer};
 use tracing::info;
 
-use crate::jwt_utils::JwtUtils;
+use crate::{jwt_utils::JwtUtils, snowflake::Snowflake};
+
+#[derive(Clone)]
+struct AppState {
+    snowflake: Arc<Mutex<Snowflake>>,
+    jwt_utils: Arc<JwtUtils>,
+}
 
 static CONFIG: OnceCell<config::Config> = OnceCell::const_new();
-static JWT_UTILS: OnceCell<Arc<JwtUtils>> = OnceCell::const_new();
 
 pub fn get_config() -> &'static config::Config {
     CONFIG.get().expect("config should be initialized")
@@ -38,67 +34,31 @@ async fn main() -> Result<(), Box<dyn Error>> {
     dotenvy::dotenv().unwrap_or_default();
     CONFIG.set(config::Config::new()).unwrap();
     let config = get_config();
-    logging::setup(&std::env::var("LOG_LEVEL").unwrap_or(String::from("info")), Some("%Y-%m-%d_%H-%M-%S.log")).unwrap();
+    logging::setup(
+        &std::env::var("LOG_LEVEL").unwrap_or(String::from("info")),
+        Some("%Y-%m-%d_%H-%M-%S.log"),
+    )
+    .unwrap();
     info!("Initializing daiwa WS server. Populating configuration");
 
     info!("Verifying JWKS");
 
-    let jwt_utils = JwtUtils::new(config.jwks_url.clone()).await;
-    if let Err(_) = JWT_UTILS.set(Arc::new(jwt_utils)) {
-        panic!("JWT_UTILS already initialized");
-    }
+    let state = AppState {
+        snowflake: Arc::new(Mutex::const_new(Snowflake::new())),
+        jwt_utils: Arc::new(JwtUtils::new(config.jwks_url.clone()).await),
+    };
 
-    let (layer, io) = SocketIo::new_layer();
+    let (layer, io) = SocketIoBuilder::new()
+        .with_state(state.clone())
+        .build_layer();
 
-    io.ns("/", events::on_connect);
+    io.ns("/", events::on_connect.with(events::verify_auth_header));
 
     let app: Router<()> = axum::Router::new()
         .layer(layer)
-        .layer(CorsLayer::new().max_age(Duration::from_secs(604800)))
-        .layer(AsyncRequireAuthorizationLayer::new(
-            |mut req: Request<Body>| async {
-                let jwt_utils = JWT_UTILS.get().unwrap().clone();
-                let headers = req.headers_mut();
-
-                let err_res = Response::builder()
-                    .status(StatusCode::UNAUTHORIZED)
-                    .body(Body::new(String::from("Unauthorized")))
-                    .unwrap_or_default();
-
-                let auth_header = match headers.get("Authorization") {
-                    Some(v) => v.to_str().unwrap_or_default(),
-                    None => return Err(err_res),
-                };
-
-                if !auth_header.starts_with("Bearer ") {
-                    return Err(err_res);
-                }
-
-                let tokens: Vec<&str> = auth_header.split(" ").collect();
-
-                if tokens.len() < 2 {
-                    return Err(err_res);
-                }
-
-                let jwt = tokens[1];
-
-                let sub = match jwt_utils.verify(jwt) {
-                    Ok(v) => v,
-                    Err(_) => return Err(err_res),
-                };
-
-                let x_user_id_value = match HeaderValue::from_str(&sub) {
-                    Ok(v) => v,
-                    Err(_) => return Err(err_res),
-                };
-
-                headers.append("X-User-ID", x_user_id_value);
-
-                Ok(req)
-            },
-        ))
+        .layer(TraceLayer::new_for_http())
         .layer(CompressionLayer::new())
-        .layer(TraceLayer::new_for_http());
+        .layer(CorsLayer::new().max_age(Duration::from_secs(604800)));
 
     info!("Starting WS server");
 
